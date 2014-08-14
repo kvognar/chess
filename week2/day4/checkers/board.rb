2635aac36422a1617f70ad5147c40d7e9a5fbb58
Kevin Vognar <kvognar@gmail.com>
2014-08-14 11:49:39 -0700

    Checkers board and piece classes

---
+++ week2/day4/checkers/board.rb
@@ -0,0 +1,81 @@
+require_relative 'piece'
+
+class Board
+  attr_reader :grid
+  
+  def initialize(empty = false)
+    @grid = Array.new(8) { Array.new(8) }
+    [:black, :red].each { |color| place_pieces(color) } unless empty
+  end
+  
+  def place_pieces(color)
+    rows = color == :black ? [0, 1, 2] : [5, 6, 7]
+    rows.each do |row|
+      8.times do |column|
+        next if (row + column).odd?
+        self[[row, column]] = Piece.new(color, [row, column], self)
+      end
+    end
+  end
+  
+  def [](pos)
+    y, x = pos
+    return nil if @grid[y].nil?
+    @grid[y][x]
+  end
+  
+  def []=(pos, element)
+    y, x = pos
+    @grid[y][x] = element
+  end
+  
+  def display
+    @grid.each do |row|
+      p row
+    end
+  end
+  
+  def move_piece(piece, pos)
+    self[piece.pos] = nil
+    self[pos] = piece
+  end
+  
+  def jump_piece(piece, pos, victim_pos)
+    self[piece.pos] = nil
+    self[pos] = piece
+    self[victim_pos] = nil
+  end
+  
+  def dup
+    all_pieces = @grid.flatten.compact.map do |piece|
+      [piece.color, piece.pos.dup, piece.king]
+    end
+    dup_board = Board.new(true)
+    all_pieces.each do |color, pos, king|
+      dup_board[pos] = Piece.new(color, pos, king, dup_board)
+    end
+    dup_board
+  end
+    
+  
+end
+
+
+
+# test jumps
+def test_jumps(jumps)
+  test_board = Board.new(true)
+  test_board[[1,1]] = Piece.new(:black, [1,1], test_board)
+  test_board[[2,2]] = Piece.new(:white, [2,2], test_board)
+  test_board[[4,2]] = Piece.new(:white, [4,2], test_board)
+  test_board[[6,2]] = Piece.new(:white, [6,2], test_board)
+  test_board.display
+  p test_board[[1,1]].moves
+  test_board[[1,1]].perform_moves(jumps)
+
+  test_board.display
+end
+
+# test_jumps([[3,3], [5,1], [7,3]])
+# test_jumps([[3,3], [6,1], [7,3]])
+# test_jumps([[2,0]])
---
+++ week2/day4/checkers/piece.rb
@@ -0,0 +1,131 @@
+# encoding: utf-8
+class Piece
+  attr_reader :pos, :color, :king
+  
+  def initialize(color, pos, king = false, board)
+    @color = color
+    @pos = pos
+    @king = king
+    @board = board
+  end
+  
+  def moves
+    slides + jumps
+  end
+  
+  def slides
+    sliding_spots = vectors.map { |dy, dx| [@pos[0] + dy, @pos[1] + dx] }
+    sliding_spots.select do |end_pos|
+      @board[end_pos].nil? && end_pos.all? {|i| i.between?(0,7) }
+    end
+  end
+  
+  def jumps
+    landing_spots = []
+    vectors.each do |dy, dx|
+      slide_spot = [@pos[0] + dy, @pos[1] + dx]
+      landing_spot = [@pos[0] + dy * 2, @pos[1] + dx * 2]
+      unless @board[slide_spot].nil? ||
+             @board[slide_spot].color == @color ||
+             landing_spot.any? { |i| !i.between?(0,7) }
+        landing_spots << landing_spot
+      end
+    end
+    landing_spots  
+  end
+  
+  def jump!(end_pos)
+    unless jumps.include?(end_pos)
+      raise InvalidMoveError.new("I can't go there")
+      return
+    end
+
+    victim_pos = middle_square(@pos, end_pos)
+    @board.jump_piece(self, end_pos, victim_pos)
+    @pos = end_pos
+    maybe_promote
+  end
+  
+  def slide!(end_pos)
+    raise "I can't go there" unless slides.include?(end_pos)
+    @board.move_piece(self, end_pos)
+    @pos = end_pos
+    maybe_promote
+  end
+  
+  def vectors
+    up = [[-1, -1], [-1, 1]]
+    down = [[1, 1], [1, -1]]
+    if @king
+      up + down
+    else
+      @color == :black ? down : up
+    end
+  end
+  
+  def to_s
+    if @king
+      @color == :black ? '✿' : '❀'
+    else
+      @color == :black ? '◉' : '◎'
+    end
+  end
+  
+  def inspect
+    to_s
+  end
+  
+  def stats
+    {"color" => :color,
+      "moves" => moves,
+      "king?" => @king,
+      "position" => pos
+    }
+  end
+  
+  def middle_square(pos1, pos2)
+    pos1.zip(pos2).map { |a, b| (a + b) / 2 }
+  end
+  
+  def maybe_promote
+    back_row = color == :black ? 7 : 0
+    @king = true if @pos[0] == back_row
+  end
+  
+  def perform_moves!(move_sequence)
+    if move_sequence.count == 1 && slides.include?(move_sequence[0])
+      slide!(move_sequence[0])
+      return
+    end 
+    move_sequence.each do |move|
+      begin
+        jump!(move)
+      rescue InvalidMoveError => e
+        puts e.message
+        break
+      end
+    end
+  end
+  
+  def perform_moves(move_sequence)
+    if valid_move_sequence?(move_sequence)
+      perform_moves!(move_sequence)
+    else
+      raise InvalidMoveError.new("Invalid move sequence!")
+    end
+  end
+  
+  def valid_move_sequence?(move_sequence)
+    begin
+      @board.dup[@pos].perform_moves!(move_sequence)
+    rescue InvalidMoveError
+      return false
+    end
+    true
+  end
+  
+  
+end
+
+class InvalidMoveError < ArgumentError
+end
