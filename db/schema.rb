9ad6ead4606f15aab1afc9988a9e5eaa4fd3b5e6
App Academy Student <student@appacademy.io>
2014-08-20 18:02:41 -0700

    Finished bonus round - custom validaiton and tagging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820222147) do
+ActiveRecord::Schema.define(version: 20140821000126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,24 @@ ActiveRecord::Schema.define(version: 20140820222147) do
 
   add_index "shortened_urls", ["short_url"], name: "index_shortened_urls_on_short_url", unique: true, using: :btree
   add_index "shortened_urls", ["user_id"], name: "index_shortened_urls_on_user_id", using: :btree
+
+  create_table "tag_topics", force: true do |t|
+    t.string   "tag_name",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tag_topics", ["tag_name"], name: "index_tag_topics_on_tag_name", using: :btree
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_topic_id",     null: false
+    t.integer  "shortened_url_id", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "taggings", ["shortened_url_id"], name: "index_taggings_on_shortened_url_id", using: :btree
+  add_index "taggings", ["tag_topic_id"], name: "index_taggings_on_tag_topic_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
