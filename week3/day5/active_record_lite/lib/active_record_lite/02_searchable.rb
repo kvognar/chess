6ac5a03df49163146004184fd8bffeb1895fcecb
Kevin Vognar <kvognar@gmail.com>
2014-08-22 16:20:35 -0700

    finished associatable_03

--- week3/day5/active_record_lite/lib/active_record_lite/02_searchable.rb
+++ week3/day5/active_record_lite/lib/active_record_lite/02_searchable.rb
@@ -3,10 +3,23 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map do |key, val|
+      "#{key} = ?"
+    end.join(" AND ")
+    
+    query = DBConnection.execute(<<-SQL, *(params.values))
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line}
+    SQL
+    self.parse_all(self.symbolized(query))
   end
 end
 
 class SQLObject
+  extend Searchable
   # Mixin Searchable here...
 end
--- week3/day5/active_record_lite/lib/active_record_lite/03_associatable.rb
+++ week3/day5/active_record_lite/lib/active_record_lite/03_associatable.rb
@@ -1,6 +1,10 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
+ActiveSupport::Inflector.inflections do |inflect|
+  inflect.irregular 'Human', 'Humans'
+end
+
 # Phase IVa
 class AssocOptions
   attr_accessor(
@@ -10,41 +14,57 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    @class_name.to_s.constantize
   end
 
   def table_name
-    # ...
+    @class_name.pluralize.underscore
   end
 end
 
 class BelongsToOptions < AssocOptions
+  attr_reader :class_name, :foreign_key, :primary_key
   def initialize(name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.capitalize
+    @foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
+  attr_reader :class_name, :foreign_key, :primary_key
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = options[:class_name] || name.to_s.capitalize.singularize
+    @foreign_key = options[:foreign_key] || "#{self_class_name.downcase.underscore}_id".to_sym
+    @primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key = self.send(options.foreign_key)
+      options.model_class.find(foreign_key)
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.name, options)
+    define_method(name) do
+      foreign_key_name = options.foreign_key
+      foreign_key = self.send(options.primary_key)
+      options.model_class.where(foreign_key_name => foreign_key)
+    end
   end
 
   def assoc_options
+    blank = self.new
     # Wait to implement this in Phase V. Modify `belongs_to`, too.
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
