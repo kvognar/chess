af52f928114d33495d92ae17564ea1492de91fd4
Kevin Vognar <kvognar@gmail.com>
2014-08-22 13:54:52 -0700

    Started ActiveRecordLite

--- week3/day5/active_record_lite/lib/active_record_lite/01_sql_object.rb
+++ week3/day5/active_record_lite/lib/active_record_lite/01_sql_object.rb
@@ -4,18 +4,40 @@ require 'active_support/inflector'
 #    of this project. It was only a warm up.
 
 class SQLObject
+    
   def self.columns
+    query_results = DBConnection.execute2(<<-SQL)
+    SELECT
+    *
+    FROM
+    #{self.table_name}
+    SQL
+    query_results.first.map(&:to_sym)
     # ...
   end
 
   def self.finalize!
+    self.columns.each do |column|
+
+      instance_variable_set("@attributes", {})
+      
+      define_method(column) do
+        @attributes[column]
+      end
+      define_method("#{column}=") do |arg|
+        @attributes[column] = arg
+      end
+    end
+
   end
 
   def self.table_name=(table_name)
+    @table_name = table_name
     # ...
   end
 
   def self.table_name
+    @table_name ||= self.to_s.pluralize.underscore
     # ...
   end
 
