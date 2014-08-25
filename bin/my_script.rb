aeae1d15964f893947c05cd607489754d20067c9
App Academy Student <student@appacademy.io>
2014-08-25 14:27:08 -0700

    first routes and controllers

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -1,11 +1,60 @@
 require 'addressable/uri'
 require 'rest-client'
 
-url = Addressable::URI.new(
+def get_users
+  url = Addressable::URI.new(
+    scheme: 'http',
+    host: 'localhost',
+    port: 3000,
+    path: '/users.html',
+    query_values: {
+    
+    } 
+  ).to_s
+
+  puts RestClient.get(url)
+end
+
+def create_user
+  url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
-  port: 3000,
-  path: '/users.html'
-).to_s
+  port: '3000',
+  path: '/users.json'
+  ).to_s
+  
+  puts RestClient.post(
+    url,
+    { user: { name: 'Frankenstein', email: "shelley@gmail.com" } }
+  )
+end
 
-puts RestClient.get(url)
+def destroy_user
+  url = Addressable::URI.new(
+  scheme: 'http',
+  host: 'localhost',
+  port: '3000',
+  path: '/users/5.html'
+  ).to_s
+  
+  puts RestClient.delete(
+    url
+    # { user: { id: 5} }
+  )
+end
+
+def update_user
+  url = Addressable::URI.new(
+  scheme: 'http',
+  host: 'localhost',
+  port: '3000',
+  path: '/users/4.html'
+  ).to_s
+  
+  puts RestClient.patch(
+    url,
+    { user: { email: "optimusprime@gmail.com"} }
+  )
+end
+
+update_user
