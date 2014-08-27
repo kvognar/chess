aafe9844bf6c18407d538737e21620c5bac322f6
App Academy Student <student@appacademy.io>
2014-08-26 17:49:53 -0700

    Added CSS

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140826184639) do
+ActiveRecord::Schema.define(version: 20140827002656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140826184639) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
   add_index "cats", ["name"], name: "index_cats_on_name", using: :btree
