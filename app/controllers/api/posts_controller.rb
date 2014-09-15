f5c909976a97e7d0fb96c6dfb33df00f6cda353f
App Academy Student <student@appacademy.io>
2014-09-15 11:54:04 -0700

    Post model and collection

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,14 @@
+class Api::PostsController < ApplicationController
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors.full_messages
+    end
+  end
+  
+  def post_params
+    params.require(:post).permit(:title,:body)
+  end
+end
