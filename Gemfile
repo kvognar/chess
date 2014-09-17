595f26eb37f92d2b88390c6a04101d6c871bd274
App Academy Student <student@appacademy.io>
2014-09-17 14:10:55 -0700

    Rebranded, made about, contact pages

--- Gemfile
+++ Gemfile
@@ -37,3 +37,4 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+gem 'bootstrap-sass'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,35 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require bootstrap
+
+$(function () {
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    $('.alert').remove();
+    $(':input').removeClass('has-warning has-error has-success');
+    
+    var anyErrors = false;
+    $(event.currentTarget).find('.form-control').each(function (index, el) {
+      var $el = $(el);
+      if ($el.val().length === 0) {
+        $el.parent().addClass("has-error");
+        anyErrors = true;
+      } else {
+        $el.parent().addClass("has-success");
+      }
+    });
+    
+    var $alertEl = $('<div></div>');
+    
+    if (anyErrors) {
+      $alertEl.addClass("alert-danger");
+      $alertEl.text("Failed submission!");
+    } else {
+      $alertEl.addClass("alert-success");
+      $alertEl.text("Successful submission!");
+    }
+    
+    $('body').append($alertEl);
+  });
+});
