fad37b0d0d9b5da890a83c3ecdcc9c84ecdc4778
Joseph Combs <joseph.e.combs@gmail.com>
2014-09-22 17:34:47 -0700

    through phase IV

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -10,4 +10,8 @@ $(function () {
   ChatApp.Chat.prototype.sendMessage = function (msg) {
     this.socket.emit('messageSubmission', { message: msg });
   };
+  
+  ChatApp.Chat.prototype.sendNicknameRequest = function(newNickname) {
+    this.socket.emit('nicknameChangeRequest', { nickname: newNickname });
+  };
 });
